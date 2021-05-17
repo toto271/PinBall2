682ac37469fafb65e4551ffa5adb69222257ff20
toto271 <82321634+toto271@users.noreply.github.com>
2021-05-17 22:00:00 +0900

    Initial commit

---
+++ Assets/kadai.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class kadai : MonoBehaviour
+{
+    private int score = 0;
+    private GameObject ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.ScoreText = GameObject.Find("ScoreText");
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "SmallStarTag")
+        {
+            this.score += 100;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 150;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || other.gameObject.tag == "LargeCloudTag")
+        {
+            this.score += 200;
+        }
+        this.ScoreText.GetComponent<Text>().text = score.ToString();
+    }
+}
